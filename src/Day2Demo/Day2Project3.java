b56eba50ecca3cfb3eb5a90136eeeb2162b66b5c
nasim <nasim@10.0.0.10>
2023-04-08 02:19:04 -0400

    added address method

--- src/Day2Demo/Day2Project3.java
+++ src/Day2Demo/Day2Project3.java
@@ -4,6 +4,15 @@ public class Day2Project3 {
 	public static void main(String[] args) {
 		
 		System.out.println("Day2 project3 HW");
+		
+		
+		Day2Project3 obj = new Day2Project3();
+		System.out.println(obj.addres());
+		
+		
+	}
+		public String addres() {
+			return "123street";
 	}
 
 }
